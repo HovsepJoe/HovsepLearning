53161a705e90afd8e28cb2ed7c261dfdc476159c
HovsepJoe <hoteyinjoseph@gmail.com>
2023-09-27 12:55:06 +0000

    did task 6

---
+++ 6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: returns 0 at success
+*/
+
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+
+printf("size of char; %lu bytes\n", size of(a));
+printf("size of int; %lu bytes\n", size of(b));
+printf("size of long int; %lu bytes\n", size of(c));
+printf("size of long long int; %lu bytes\n", size of(d));
+printf("size of float; %lu bytes\n", size of(f));
+
+return (0);
+}
