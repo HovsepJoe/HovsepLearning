8482b12af058921832b27b58f10ae29a9712e1a5
HovsepJoe <hoteyinjoseph@gmail.com>
2023-09-27 13:00:24 +0000

    did task 6

--- 6-size.c
+++ 6-size.c
@@ -14,11 +14,11 @@ long int c;
 long long int d;
 float f;
 
-printf("size of char; %lu bytes\n", size of(a));
-printf("size of int; %lu bytes\n", size of(b));
-printf("size of long int; %lu bytes\n", size of(c));
-printf("size of long long int; %lu bytes\n", size of(d));
-printf("size of float; %lu bytes\n", size of(f));
+printf("size of char; %lu bytes\n", sizeof(a));
+printf("size of int; %lu bytes\n", sizeof(b));
+printf("size of long int; %lu bytes\n", sizeof(c));
+printf("size of long long int; %lu bytes\n", sizeof(d));
+printf("size of float; %lu bytes\n", sizeof(f));
 
 return (0);
 }
